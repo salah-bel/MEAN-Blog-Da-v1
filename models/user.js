63d219bbf6e702aca102a46de43b8b50bcd843af
salah bel <belasalah.com>
2020-10-10 16:42:18 +0200

    register validation: hash password, RegExp validation of Password, Username and email

--- models/user.js
+++ models/user.js
@@ -1,10 +1,155 @@
- const mongoose = require('mongoose');
- const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-  const userSchema = new Schema({
-    email:      {type: String, required: true, unique: true, lowercase: true},
-    username:   {type: String, required: true, unique: true, lowercase: true},
-    password:   {type: String, required: true},
+// EMAIL VALIDATION
+// 1-- validation Email: check length of email
+const emailLenghtChecker = email => {
+  if (!email) {
+    return false;
+  }else {
+    if (email.length < 5 || email.length >30 ) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+}
+// 2--validation Email: RegExp
+const validEmailChecker = email => {
+  if (!email) {
+    return false
+  }else {
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regExp.test(email);
+  }
+}
+// 3-- validation array Email
+const emailValidators = [
+  {
+    validator: emailLenghtChecker,
+    msg: 'E-mail must be between 5 and 30 characters'
+  },
+  {
+    validator: validEmailChecker,
+    msg: 'Must be a valid Email'
+  }
+]
+
+
+// USERNAME VALIDATION
+// 1-- validation username: lengths
+const usernameLenghtChecker = username => {
+  if (!username) {
+    return false;
+  }else {
+    if (username.length < 5 || username.length >15 ) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+} 
+// 2--validation username: RegExp
+const validUsernameChecker = username => {
+  if (!username) {
+    return false
+  }else {
+    const regExp = new RegExp( /^[a-zA-Z\-]+$/);
+    return regExp.test(username);
+  }
+}
+// 3-- validation array Username
+const usernameValidators = [
+  {
+    validator: usernameLenghtChecker,
+    msg: 'Username must be between 5 and 15 characters'
+  },
+  {
+    validator: validUsernameChecker,
+    msg: 'Must be a valid Username'
+  }
+]
+
+// PASSWORD VALIDATION
+// 1-- validation password: lengths
+const passwordLenghtChecker = password => {
+  if (!password) {
+    return false;
+  }else {
+    if (password.length < 8 || password.length >30 ) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+} 
+// 2--validation password: RegExp
+const validPasswordChecker = password => {
+  if (!password) {
+    return false
+  }else {
+    const regExp = new RegExp( "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})" );
+    return regExp.test(password);
+  }
+}
+// 3-- validation array password
+const passwordValidators = [
+  {
+    validator: passwordLenghtChecker,
+    msg: 'Username must have 8 charactres min  and 30 characters max'
+   },
+  {
+    validator: validPasswordChecker,
+    msg: 'Password must have at least one Uppercase, Lowercase, special charactre, and number'
+  }
+]
+
+
+
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
+  username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
+  password: { type: String, required: true, validate: passwordValidators },
+});
+
+
+//midellware
+userSchema.pre('save', function (next) {
+  const user = this;
+  const salt = 10;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(salt, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
   });
+});
+//compare password
+
+userSchema.methods.comparePassword = password => {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) {
+      console.log(err)
+    } else {
+      isMatch
+    };
+
+  });
+};
+
+
+
 
 module.exports = mongoose.model('User', userSchema);
--- routes/authentication.js
+++ routes/authentication.js
@@ -4,27 +4,47 @@ module.exports = (router) => {
     router.post('/register', (req, res) => {
         if (!req.body.email) {
             res.json({ success: false, msg: 'must provid Email' })
-        } else {
+            } else {
             if (!req.body.username) {
                 res.json({ success: false, msg: 'must provid Username' })
             } else {
                 if (!req.body.password) {
                     res.json({ success: false, msg: 'must provid Password' })
                 } else {
+
+                    // create user  
                     let user = new User({
                         email: req.body.email,
                         username: req.body.username,
                         password: req.body.password
                     });
                     // save user
-                    user.save((err) => {
+                    user.save(err => {
                         if (err) {
-                            res.json({ success: false, msg: ' can not save user. Error:', err });
-                            console.log(err)
-                        } else {
-                            res.json({ success: true, msg: ' User saved successfuly' });
-
+                            
+                            if (err.code === 11000) {
+                                res.json({success: false, msg: 'username or password already exist!'}) 
+                            }else {
+                                if (err.errors) {
+                                    if (err.errors.email) {
+                                        res.json({success: false, msg: err.errors.email.message})
+                                    }else {
+                                        if (err.errors.username) {
+                                            res.json({success: false, msg: err.errors.username.message})
+                                        }else {
+                                            if (err.errors.password) {
+                                                res.json({success: false, msg: err.errors.password.message})
+                                            } else {
+                                                res.json({success: false, msg: err})
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }else {
+                            res.json({success: true, msg: 'User saved successfuly!'})
                         }
+                       
                     });
                 }
             }
